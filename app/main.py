a7d33b33535883b21d0989a7cdf9804e21da0dad
syednabilashraf <nabilashrafofficial@gmail.com>
2023-03-05 19:47:55 +0100

    parse and write back commands

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
+from io import StringIO
+import io
 import socket
+
+from app.resp import BulkString, ErrorString, RESPArray, RESPValue, SimpleString, parse_resp_value, serialize_resp_value
 
 
 def print_prefix_lines(text: str, prefix: str):
     print(*(prefix + line for line in text.splitlines()), sep="\n")
+
+
+def execute_command(command: str) -> RESPValue:
+    if command == "PING":
+        return SimpleString("PONG")
+    else:
+        return ErrorString(f"ERR Unsupported command: {command}")
+
+
+def execute_request(query: io.TextIOBase) -> bytes:
+    request = parse_resp_value(query)
+    print("request", request)
+    if not isinstance(request, RESPArray):
+        raise Exception("Expected array containing user requests")
+    all_results = bytes()
+    for command in request:
+        if not isinstance(command, BulkString):
+            raise Exception("Expected string command")
+        command = command.upper()
+        result = execute_command(command)
+        print("command", command, "result", result)
+        all_results = all_results + serialize_resp_value(result).encode()
+    return all_results
 
 
 def main():
@@ -14,8 +41,13 @@ def main():
         query = client_connection.recv(1024).decode()
         if not query:
             break
-        print_prefix_lines(query, "client:")
-        client_connection.send(b"+PONG\r\n")
+        # print_prefix_lines(query, "client:")
+
+        while query:
+            query_stream = StringIO(query)
+            result = execute_request(query_stream)
+            client_connection.send(result)
+            query = query_stream.read()
 
 
 if __name__ == "__main__":
---
+++ app/resp.py
@@ -0,0 +1,56 @@
+from io import TextIOBase
+
+
+class SimpleString(str):
+    def __repr__(self) -> str:
+        return "Simple(" + super().__repr__() + ")"
+
+
+class ErrorString(str):
+    def __repr__(self) -> str:
+        return "Error(" + super().__repr__() + ")"
+
+
+class BulkString(str):
+    def __repr__(self) -> str:
+        return "Bulk(" + super().__repr__() + ")"
+
+
+class RESPArray(list["RESPValue"]):
+    def __repr__(self) -> str:
+        return "RESPArray(" + super().__repr__() + ")"
+
+
+RESPValue = SimpleString | BulkString | ErrorString | RESPArray
+
+
+def parse_resp_value(data: TextIOBase) -> RESPValue:
+    # if isinstance(data, StringIO):
+    #     print("data:", repr(data.getvalue()))
+    match data.read(1):
+        case "+":
+            return SimpleString(data.readline())
+        case "-":
+            return ErrorString(data.readline())
+        case "$":
+            length = int(data.readline())
+            text = data.read(length)
+            assert data.read(2) == "\r\n"
+            return BulkString(text)
+        case "*":
+            length = int(data.readline())
+            return RESPArray([parse_resp_value(data) for _ in range(length)])
+        case c:
+            raise Exception("Unsupported request:", c+data.readline())
+
+
+def serialize_resp_value(value: RESPValue) -> str:
+    match value:
+        case SimpleString(s):
+            return f"+{s}\r\n"
+        case ErrorString(s):
+            return f"-{s}\r\n"
+        case BulkString(s):
+            return f"${len(s)}\r\n{s}\r\n"
+        case RESPArray(a):
+            return f"*{len(a)}\r\n" + "".join(map(serialize_resp_value, a))
